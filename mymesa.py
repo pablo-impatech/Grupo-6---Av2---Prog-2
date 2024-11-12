da6864c3ae86efdb6f23491fce31b5b26d2b63a7
Igor-Roberto-Alves <al.igor.alves@impatech.org.br>
2024-11-12 10:43:23 -0300

    ✨ feat(mymesa.py) - Fireman

--- mymesa.py
+++ mymesa.py
@@ -3,6 +3,7 @@ import random
 import pygame
 import time
 import matplotlib.pyplot as plt
+import agents as agent
 
 # Inicialização do Pygame
 pygame.init()
@@ -35,11 +36,19 @@ TREE_ALIVE_IMG = pygame.transform.scale(TREE_ALIVE_IMG, (cell_size, cell_size))
 TREE_BURNING_IMG = pygame.transform.scale(TREE_BURNING_IMG, (cell_size, cell_size))
 WATER_IMG = pygame.transform.scale(WATER_IMG, (cell_size, cell_size))
 TREE_BURNED_IMG = pygame.transform.scale(TREE_BURNED_IMG, (cell_size, cell_size))
-BUTTOM_UP_IMG = pygame.transform.scale(BUTTOM_UP_IMG, (cell_size, cell_size))
-BUTTOM_LEFT_IMG = pygame.transform.scale(BUTTOM_LEFT_IMG, (cell_size, cell_size))
-BUTTOM_DOWN_IMG = pygame.transform.scale(BUTTOM_DOWN_IMG, (cell_size, cell_size))
-BUTTOM_RIGHT_IMG = pygame.transform.scale(BUTTOM_RIGHT_IMG, (cell_size, cell_size))
-BUTTOM_X_IMG = pygame.transform.scale(BUTTOM_X_IMG, (cell_size, cell_size))
+BUTTOM_UP_IMG = pygame.transform.scale(
+    BUTTOM_UP_IMG, (1.5 * cell_size, 1.5 * cell_size)
+)
+BUTTOM_LEFT_IMG = pygame.transform.scale(
+    BUTTOM_LEFT_IMG, (1.5 * cell_size, 1.5 * cell_size)
+)
+BUTTOM_DOWN_IMG = pygame.transform.scale(
+    BUTTOM_DOWN_IMG, (1.5 * cell_size, 1.5 * cell_size)
+)
+BUTTOM_RIGHT_IMG = pygame.transform.scale(
+    BUTTOM_RIGHT_IMG, (1.5 * cell_size, 1.5 * cell_size)
+)
+BUTTOM_X_IMG = pygame.transform.scale(BUTTOM_X_IMG, (1.5 * cell_size, 1.5 * cell_size))
 FIREMAN_IMG = pygame.transform.scale(FIREMAN_IMG, (1.5 * cell_size, 1.5 * cell_size))
 FIREMAN_BURNING1_IMG = pygame.transform.scale(
     FIREMAN_BURNING1_IMG, (1.5 * cell_size, 1.5 * cell_size)
@@ -49,298 +58,19 @@ FIREMAN_BURNING2_IMG = pygame.transform.scale(
 )
 
 
-def neighbors(self, matriz):
-    lista = []
-    directions = [
-        (0, 1),
-        (1, 0),
-        (-1, 0),
-        (0, -1),
-        (1, 1),
-        (1, -1),
-        (-1, 1),
-        (-1, -1),
-    ]
-    for dx, dy in directions:
-        nx, ny = self[0] + dx, self[1] + dy
-        if (
-            0 <= nx < len(matriz)
-            and 0 <= ny < len(matriz[0])
-            and isinstance(matriz[nx][ny], Tree)
-        ):
-            lista.append(matriz[nx][ny])
-    return lista
-
-
-class bombeiro:
-    def __init__(self, matriz):
-        self.step = 0
-        self.matriz = matriz
-        self.life = 1
-        self.status = "alive"
-        while True:
-            self.x, self.y = random.randint(0, len(matriz) - 1), random.randint(
-                0, len(matriz[0]) - 1
-            )
-            if isinstance(matriz[self.x][self.y], Tree):
-                break
-
-    def andar(self):
-        self.step += 1
-        if self.step == 5:
-            direction = [
-                (0, 1),
-                (0, -1),
-                (1, 0),
-                (-1, 0),
-                (1, 1),
-                (-1, 1),
-                (1, -1),
-                (-1, -1),
-            ]
-            random.shuffle(direction)
-            directions_possi = []
-            for dx, dy in direction:
-                nx, ny = self.x + dx, self.y + dy
-                if (
-                    0 <= nx < len(self.matriz)
-                    and 0 <= ny < len(self.matriz[0])
-                    and (
-                        isinstance(self.matriz[nx][ny], Tree)
-                        or self.matriz[nx][ny] == "v"
-                    )
-                ):
-                    self.x, self.y = nx, ny  # Move o bombeiro para a nova posição
-                    directions_possi.append((nx, ny))
-
-            if directions_possi:
-                a = random.choice(directions_possi)
-                self.x, self.y = a[0], a[1]
-
-            self.step = 0
-
-    """def probability_atualization(self):
-        for neigh in neighbors([self.x, self.y], self.matriz):
-            if isinstance(neigh, Tree):
-                neigh.density = 1"""
-    """
-    é preciso determinar como o bombeiro influenciará as árvores ao redor.
-    -> as árvores ao seu redor aumentam de densidade, ou seja tem menos chance de pegar fogo
-    -> a cada frame o bombeiro retorna uma condição da árvore,
-    Se a segunda for escolhida é necessário que as árvores guardem um self.previous_condition
-    
-    """
-
-    def atualizar_bombeiro(self):
-        for neigh in neighbors((self.x, self.y), self.matriz):
-            if neigh.condition == "burning":
-                self.life -= 0.01
-
-        if 0.5 <= self.life <= 0.8:
-            self.status = "burning"
-
-        elif 0 < self.life < 0.5:
-            self.status = "burning2"
-
-        elif self.life <= 0:
-            self.status = "dead"
-
-
-class buttom:
-    def __init__(self, x, y, width, height):
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        self.visible = True
-
-    def is_button_clicked(self, pos):
-        if self.visible:
-            return (
-                self.x <= pos[0] <= self.x + self.width
-                and self.y <= pos[1] <= self.y + self.height
-            )
-        else:
-            return None
-
-
-class Tree:
-    def __init__(self, coord):
-        self.condition = "alive"
-        self.density = random.randint(50, 80)
-        self.next_condition = None
-        self.x = coord[0]
-        self.y = coord[1]
-        self.count = 0
-        self.step = 0
-
-    def neighbors(self, matriz):
-        lista = []
-        directions = [
-            (0, 1),
-            (1, 0),
-            (-1, 0),
-            (0, -1),
-            (1, 1),
-            (1, -1),
-            (-1, 1),
-            (-1, -1),
-        ]
-        for dx, dy in directions:
-            nx, ny = self.x + dx, self.y + dy
-            if (
-                0 <= nx < len(matriz)
-                and 0 <= ny < len(matriz[0])
-                and isinstance(matriz[nx][ny], Tree)
-            ):
-                lista.append(matriz[nx][ny])
-        return lista
-
-    def attempt_to_burn(self, matriz, vent):
-
-        if self.condition == "alive":
-            self.next_condition = "burning"
-            for neighbor in self.neighbors(matriz):
-                if (
-                    neighbor.condition == "alive"
-                    and neighbor.next_condition != "burning"
-                ):
-                    probability = 100 - neighbor.density
-                    if vent.directions:
-                        if neighbor in vent.neighbors_vento(self, matriz):
-                            probability = min(80, probability + 30)
-                            probability = 100  # Probabilidade determinada assim para melhorar a visualização do vento
-                        else:
-                            probability = max(40, probability - 20)
-                            probability = 0
-                    if (
-                        random.random() < probability / 100
-                    ):  # queima o vizinho com probabilidade 1 - densidade da árvore
-                        neighbor.next_condition = "tick"
-
-    def burn_continuos(self, matriz, vent):
-        for neighbor in self.neighbors(matriz):
-            if neighbor.condition == "alive" and neighbor.next_condition != "burning":
-                probability = min(100 - neighbor.density - 40, 10)
-                if vent.directions:
-                    if neighbor in vent.neighbors_vento(self, matriz):
-                        probability = min(60, probability + 10)
-                        probability = 100  # Probabilidade determinada assim para melhorar a visualização do vento
-                    else:
-                        probability = max(10, probability - 20)
-                        probability = 0
-                if (
-                    random.random() < probability / 100
-                ):  # queima o vizinho com probabilidade 1 - densidade da árvore
-                    neighbor.next_condition = "tick"
-
-    def update_condition(self, forest):
-        matriz = forest.matriz
-        vent = forest.vent
-        if self.next_condition == "burned":
-            self.next_condition = "step"
-            self.condition = "burned"
-            self.burn_continuos(matriz, vent)
-
-        elif self.next_condition == "step":
-            self.step += 1
-            self.burn_continuos(matriz, vent)
-            if self.step == 3:
-                self.next_condition = "final"
-
-        elif self.next_condition == "final":
-            matriz[self.x][self.y] = "v"
-
-        if self.next_condition == "burning":  # Se o próximo estágio é queimando
-            self.attempt_to_burn(matriz, vent)  # queima os vizinhos
-            self.count += 1
-            self.condition = self.next_condition
-            if self.count == 4:
-                self.next_condition = "burned"
-
-        if (
-            self.next_condition == "tick"
-        ):  # tick pode ser usado para melhorar a transição entre alive e burning
-            self.next_condition = "burning"
-
-    def __repr__(self):  # para visualizar a matriz
-        if self.condition == "alive":
-            return "1"
-        if self.condition == "burning":
-            return "b"
-        if self.condition == "burned":
-            return "0"
-
-
-class Barrier:  # representará barreiras como água ou muro, algo assim
-    def __init__(self, coord):
-        self.x = coord[0]
-        self.y = coord[1]
-
-    def __repr__(self):
-        return "a"
-
-
-class Houses:
-    def __init__(self, coord):
-        self.x = coord[0]
-        self.y = coord[0]
-
-    def __repr__(self):
-        return "h"
-
-
-class vento:
-    def __init__(self, direction=None):
-        lista_directions = ["N", "S", "L", "O", "NO", "NE", "SE", "SO"]
-        self.directions = []
-        if direction == 1:
-            direction = random.choice(lista_directions)
-        if direction == "L":
-            self.directions = [(0, 1), (1, 1), (-1, 1)]
-        elif direction == "O":
-            self.directions = [(0, -1), (1, -1), (-1, -1)]
-        elif direction == "S":
-            self.directions = [(1, 0), (1, 1), (1, -1)]
-        elif direction == "N":
-            self.directions = [(-1, 0), (-1, 1), (-1, -1)]
-        elif direction == "SE":
-            self.directions = [(0, 1), (1, 0), (1, 1)]
-        elif direction == "NE":
-            self.directions = [(0, 1), (-1, 0), (-1, 1)]
-        elif direction == "SO":
-            self.directions = [(0, -1), (1, 0), (1, -1)]
-        elif direction == "NO":
-            self.directions = [(0, -1), (-1, 0), (-1, -1)]
-
-    def neighbors_vento(self, tree, matriz):
-        lista = []
-        if self.directions:
-            for dx, dy in self.directions:
-                nx, ny = tree.x + dx, tree.y + dy
-                if (
-                    0 <= nx < len(matriz)
-                    and 0 <= ny < len(matriz[0])
-                    and isinstance(matriz[nx][ny], Tree)
-                ):
-                    lista.append(matriz[nx][ny])
-
-        return lista
-
-
 class Forest:
     def __init__(self, matriz):
         self.matriz = matriz
         self.n = len(matriz)
         self.m = len(matriz[0])
-        self.vent = vento(1)
+        self.vent = agent.vento()
 
     def incendio(self):
         for i in range(5):
             k = random.randint(0, self.n - 1)
             l = random.randint(0, self.m - 1)
             if (
-                isinstance(self.matriz[k][l], Tree)
+                isinstance(self.matriz[k][l], agent.Tree)
                 and self.matriz[k][l].condition == "alive"
             ):
                 self.matriz[k][l].attempt_to_burn(self.matriz, self.vent)
@@ -349,14 +79,14 @@ class Forest:
     def update_forest(self):
         for row in self.matriz:
             for cell in row:
-                if isinstance(cell, Tree):
+                if isinstance(cell, agent.Tree):
                     cell.update_condition(self)
 
     def extinguish_tree_at(self, x, y):
         col, row = x // cell_size, y // cell_size
         if 0 <= row < self.n and 0 <= col < self.m:
             cell = self.matriz[row][col]
-            if isinstance(cell, Tree):
+            if isinstance(cell, agent.Tree):
                 cell.extinguish_fire()
 
     def surge_trees(self):
@@ -365,10 +95,10 @@ class Forest:
                 if self.matriz[i][j] == "v":
                     a = random.randint(1, 100)
                     if a == 1:
-                        self.matriz[i][j] = Tree((i, j))
+                        self.matriz[i][j] = agent.Tree((i, j))
 
     def tree_f_pos(self, coord):
-        self.matriz[coord[0]][coord[1]] == Tree((coord[0], coord[1]))
+        self.matriz[coord[0]][coord[1]] == agent.Tree((coord[0], coord[1]))
 
 
 def draw_forest(screen, forest):
@@ -376,14 +106,14 @@ def draw_forest(screen, forest):
     for i in range(forest.n):
         for j in range(forest.m):
             cell = forest.matriz[i][j]
-            if isinstance(cell, Tree):
+            if isinstance(cell, agent.Tree):
                 if cell.condition == "alive":
                     screen.blit(TREE_ALIVE_IMG, (j * cell_size, i * cell_size))
                 elif cell.condition == "burning":
                     screen.blit(TREE_BURNING_IMG, (j * cell_size, i * cell_size))
                 elif cell.condition == "burned":
                     screen.blit(TREE_BURNED_IMG, (j * cell_size, i * cell_size))
-            elif isinstance(cell, Barrier):
+            elif isinstance(cell, agent.Barrier):
                 screen.blit(WATER_IMG, (j * cell_size, i * cell_size))
             if cell == "v":
                 pygame.draw.rect(
@@ -408,13 +138,15 @@ def main():
     def init_screen():
         screen = pygame.display.set_mode((tela_x, tela_y))
         matriz = [
-            [Tree((i, j)) for j in range(tela_x // cell_size)]
+            [agent.Tree((i, j)) for j in range(tela_x // cell_size)]
             for i in range((tela_y // cell_size))
         ]
 
         for i in range((tela_x) // int(0.002 * tela_x * cell_size)):
             for j in range(tela_y // cell_size):
                 matriz[j][i] = "black"
+
+        matriz[15][38] = agent.Barrier((23, 78))
 
         return matriz, screen
 
@@ -438,7 +170,7 @@ def main():
                 )
 
     forest = Forest(matriz)  # Inicializando a Floresta
-    forest.vent = vento()
+    forest.vent = agent.vento()
     alive_burning_burned = (
         []
     )  # Carregará número de árvores vivas, queimando e queimadas para gerar o gráfico
@@ -447,34 +179,34 @@ def main():
     # Criando os botões
     button_width, button_height = START_IMG.get_width(), START_IMG.get_height()
     button_x, button_y = tela_x // 2, tela_y // 2
-    start_but = buttom(button_x, button_y, button_width, button_height)
+    start_but = agent.buttom(button_x, button_y, button_width, button_height)
 
     button_x, button_y = tela_x // 10, 0.42 * tela_y
     button_width, button_height = BUTTOM_UP_IMG.get_width(), BUTTOM_UP_IMG.get_height()
-    up_but = buttom(button_x, button_y, button_width, button_height)
+    up_but = agent.buttom(button_x, button_y, button_width, button_height)
 
     button_y = 0.525 * tela_y
-    down_but = buttom(button_x, button_y, button_width, button_height)
+    down_but = agent.buttom(button_x, button_y, button_width, button_height)
     button_x = 0.07 * tela_x
     button_y = 0.475 * tela_y
-    left_but = buttom(button_x, button_y, button_width, button_height)
+    left_but = agent.buttom(button_x, button_y, button_width, button_height)
     button_x = 0.13 * tela_x
-    right_but = buttom(button_x, button_y, button_width, button_height)
+    right_but = agent.buttom(button_x, button_y, button_width, button_height)
     button_x = 0.1 * tela_x
-    x_but = buttom(button_x, button_y, button_width, button_height)
+    x_but = agent.buttom(button_x, button_y, button_width, button_height)
     button_x, button_y = 0.003 * tela_x * cell_size, 0.03 * tela_y * cell_size
     button_width, button_height = (
         BUTTOM_PAUSE_IMG.get_width(),
         BUTTOM_PAUSE_IMG.get_height(),
     )
-    pause_but = buttom(button_x, button_y, button_width, button_height)
+    pause_but = agent.buttom(button_x, button_y, button_width, button_height)
 
     start = False  # Controle para verificar se o incêndio deve iniciar
     aux = False
     start2 = False
     loading = True
     pause_but.visible = False
-    bombeiros = [bombeiro(matriz) for _ in range(5)]
+    bombeiros = [agent.bombeiro(matriz) for _ in range(5)]
     bombeiros_andar = False
 
     while running:
@@ -495,23 +227,23 @@ def main():
 
                 if up_but.is_button_clicked(event.pos):
                     print("up clicado")
-                    forest.vent = vento("N")
+                    forest.vent = agent.vento("N")
 
                 if down_but.is_button_clicked(event.pos):
                     print("down clicado")
-                    forest.vent = vento("S")
+                    forest.vent = agent.vento("S")
 
                 if right_but.is_button_clicked(event.pos):
                     print("right clicado")
-                    forest.vent = vento("L")
+                    forest.vent = agent.vento("L")
 
                 if left_but.is_button_clicked(event.pos):
                     print("left clicado")
-                    forest.vent = vento("O")
+                    forest.vent = agent.vento("O")
 
                 if x_but.is_button_clicked(event.pos):
                     print("x clicado")
-                    forest.vent = vento()
+                    forest.vent = agent.vento()
 
                 if pause_but.is_button_clicked(event.pos):
                     print("pause clicado")
@@ -561,7 +293,7 @@ def main():
             for cell in row:
                 if cell == "v":
                     burned += 1
-                if isinstance(cell, Tree):
+                if isinstance(cell, agent.Tree):
                     if cell.condition == "alive":
                         alive += 1
 
